33d7e1fa87fdf54c6ffee18d23322a1d933c095e
jp8002 <162762832+jp8002@users.noreply.github.com>
2024-06-16 23:29:50 +0000

    Add files via upload

--- Site/scripts/nosso.js
+++ Site/scripts/nosso.js
@@ -1,20 +1,36 @@
 /*Função que realiza a mudança para o modo claro*/
-    function lightMode() { 
-        var element = document.body;
-        var claro = document.getElementById("icone-claro");
-        var escuro = document.getElementById("icone-escuro");
-        element.classList.add("lightMode");
-        claro.style.display="block";
-        escuro.style.display="none";
-    }
+function lightMode() { 
+  var element = document.body;
+  var claro = document.getElementById("icone-claro");
+  var escuro = document.getElementById("icone-escuro");
+  var claroi = document.getElementById("icone-claro-inferior");
+  var escuroi = document.getElementById("icone-escuro-inferior");
 
-    /*Função que realiza a mudança para o modo escuro*/
-    function darkMode(){
-      var element = document.body;
-      var claro = document.getElementById("icone-claro");
-      var escuro = document.getElementById("icone-escuro");
-      element.classList.remove("lightMode");
-      claro.style.display="none";
-      escuro.style.display="block";
-    }
-  
+  element.classList.add("lightMode");
+  claro.style.display="block";
+  claroi.style.display="none";
+
+  escuroi.style.display="block";
+  escuro.style.display="none";
+}
+
+/*Função que realiza a mudança para o modo escuro*/
+function darkMode(){
+  var element = document.body;
+  var claro = document.getElementById("icone-claro");
+  var escuro = document.getElementById("icone-escuro");
+  var claroi = document.getElementById("icone-claro-inferior");
+  var escuroi = document.getElementById("icone-escuro-inferior");
+
+  element.classList.remove("lightMode");
+  claro.style.display="none";
+  claroi.style.display="block";
+
+  escuroi.style.display="none";
+  escuro.style.display="block";
+}
+
+function settings(){
+  var teste = document.getElementById("troca");
+  teste.classList.toggle("ativo");
+}
